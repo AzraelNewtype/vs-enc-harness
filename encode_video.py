32f068d0a4f0ac1e56fb89cdcb58a0798b4b9e71
Chris <c.a.eads@gmail.com>
2016-08-06 03:21:21 -0400

    Migrated load_settings and get_vid_info to videoutilities.py also
    Added necessary imports to videoutilities.py so it doesn't complain anymore.

--- encode_video.py
+++ encode_video.py
@@ -9,36 +9,11 @@ import subprocess
 import sys
 import videoutilities as vu
 
-try:
-    import yaml
-except ImportError:
-    sys.exit("You need to install PyYaml for this to work.")
-
 
 class Opts(object):
     pass
 
-
-def load_settings(series):
-    try:
-        script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
-        yaml_loc = os.path.join(script_dir,"encoder.yaml")
-        with open(yaml_loc) as y:
-            all_settings = yaml.load(y)
-    except IOError:
-        sys.exit("Cannot load encoder.yaml, cannot continue.")
-
-    settings = all_settings['Global']
-    try:
-        settings.update(all_settings['Series'][series])
-    except KeyError:
-        print('No entry for series "{0}" in encoder.yaml, the available options are:'.format(series))
-        for series in all_settings['Series']:
-            print(series)
-        raise SystemExit
-
-    return settings
-
+    
 def fix_windows_paths(path_in):
     if path_in:
         return re.sub(r'\\', '/', path_in)
@@ -46,20 +21,6 @@ def fix_windows_paths(path_in):
 def display_windows_paths(cmd_in):
     return re.sub('/', r'\\', cmd_in)
 
-def get_vid_info(settings):
-    type = settings['type']
-    info_cmd =  '{0} -i -a "type={1}" -a "src_vid={2}" {3} -'.format(
-        settings['vspipe'], type, settings['vid_in'], settings['script_in'])
-    iargs = shlex.split(info_cmd)
-    finfo_raw = subprocess.check_output(iargs)
-    finfo_raw = finfo_raw.decode('utf-8')
-    finfo_lines = finfo_raw.split('\n')
-    finfo = {}
-    for line in finfo_lines:
-        parts = line.split(": ")
-        if len(parts) > 1:
-            finfo[parts[0]] = parts[1]
-    return finfo
     
 # c:\enc_tools\vspipe64.exe -i -a type=hd -a "src_vid=c:\\users\\chris\\documents\\gobus\\test_lossless.mp4" c:\\users\\chris\\documents\\gobus\\test-out.vpy -
 
@@ -79,7 +40,7 @@ def encode_video(settings):
     else:
         encoder = settings['x264_10']
     enc_opts = settings[type + '_opts'].strip()
-    frame_info = get_vid_info(settings)
+    frame_info = vu.get_vid_info(settings)
     enc_cmd = "{0} {1} --demuxer y4m - -o {2} --frames {3}".format(
         encoder, enc_opts, settings['vid_out'], frame_info['Frames'])
     if settings['qpfile']:
@@ -127,7 +88,7 @@ if __name__ == "__main__":
     args = parser.parse_args(namespace=Opts)
     
     # Grab the settings from the yaml based on input
-    settings = load_settings(Opts.series)
+    settings = vu.load_settings(Opts.series)
     
     settings['pretend'] = Opts.pretend
     settings['tc'] = Opts.tc
--- videoutilities.py
+++ videoutilities.py
@@ -1,5 +1,50 @@
+import os
+import shlex
+import subprocess
+import sys
+try:
+    import yaml
+except ImportError:
+    sys.exit("You need to install PyYaml for this to work.")
+
+
 def preprin(foo):
     """Debug printing. It's not worth importing always, and lazier to type as needed."""
     import pprint
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(foo)
+    
+def load_settings(series):
+    try:
+        script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        yaml_loc = os.path.join(script_dir,"encoder.yaml")
+        with open(yaml_loc) as y:
+            all_settings = yaml.load(y)
+    except IOError:
+        sys.exit("Cannot load encoder.yaml, cannot continue.")
+
+    settings = all_settings['Global']
+    try:
+        settings.update(all_settings['Series'][series])
+    except KeyError:
+        print('No entry for series "{0}" in encoder.yaml, the available options are:'.format(series))
+        for series in all_settings['Series']:
+            print(series)
+        raise SystemExit
+
+    return settings
+    
+def get_vid_info(settings):
+    type = settings['type']
+    info_cmd =  '{0} -i -a "type={1}" -a "src_vid={2}" {3} -'.format(
+        settings['vspipe'], type, settings['vid_in'], settings['script_in'])
+    iargs = shlex.split(info_cmd)
+    finfo_raw = subprocess.check_output(iargs)
+    finfo_raw = finfo_raw.decode('utf-8')
+    finfo_lines = finfo_raw.split('\n')
+    finfo = {}
+    for line in finfo_lines:
+        parts = line.split(": ")
+        if len(parts) > 1:
+            finfo[parts[0]] = parts[1]
+    return finfo
