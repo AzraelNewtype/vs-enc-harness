8c79ea312280b71d10086c81913077e4aa02b632
Chris <c.a.eads@gmail.com>
2016-08-09 22:30:16 -0400

    Add load_global_settings()
    Returns a truncated version of the settings, essentially just for ts_demux_audio.py, which really only needs one item, but there is a config file so why wouldn't I store config data there

--- videoutilities.py
+++ videoutilities.py
@@ -16,6 +16,7 @@ def preprin(foo):
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(foo)
     
+    
 def load_settings(series):
     try:
         script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -36,6 +37,19 @@ def load_settings(series):
 
     return settings
     
+    
+def load_global_settings():
+    try:
+        script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        yaml_loc = os.path.join(script_dir,"encoder.yaml")
+        with open(yaml_loc) as y:
+            all_settings = yaml.load(y)
+    except IOError:
+        sys.exit("Cannot load encoder.yaml, cannot continue.")
+        
+    return all_settings['Global']
+    
+    
 def get_vid_info(settings):
     type = settings['type']
     info_cmd =  '{0} -i -a "type={1}" -a "src_vid={2}" {3} -'.format(
@@ -51,6 +65,7 @@ def get_vid_info(settings):
             finfo[parts[0]] = parts[1]
     return finfo
     
+    
 def get_arbitrary_vid_info(settings):
     vload_str = "import vapoursynth as vs\nc = vs.get_core()\nv_in = c.lsmas.LWLibavSource(src_vid, cache=1)\nv_in.set_output()"
     try:
@@ -61,18 +76,21 @@ def get_arbitrary_vid_info(settings):
         settings['vid_in'] = os.path.normpath(settings['vid_in'])
     else:
         settings['vid_in'] = os.path.abspath(settings['vid_in'])
-    vload = tempfile.NamedTemporaryFile('w+', delete=False)
-    vload.write(vload_str)
-    vload.close()
-    if os.name == 'nt':
-        settings['script_in'] = fix_windows_paths(vload.name)
-    else:
-        settings['script_in'] = vload.name
-    finfo = get_vid_info(settings)
-    os.remove(vload.name)
+    try:
+        vload = tempfile.NamedTemporaryFile('w+', delete=False)
+        vload.write(vload_str)
+        vload.close()
+        if os.name == 'nt':
+            settings['script_in'] = fix_windows_paths(vload.name)
+        else:
+            settings['script_in'] = vload.name
+        finfo = get_vid_info(settings)
+    finally:
+        os.unlink(vload.name)
     settings['script_in'] = old_script
     return finfo
 
+    
 def fix_windows_paths(path_in):
     if path_in:
         return re.sub(r'\\', '/', path_in)
